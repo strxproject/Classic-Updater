19369e7513e3e072d231d776046c7a3dc4f54991
patricktbp <135556230+n1d3v@users.noreply.github.com>
2024-07-24 23:19:07 +0100

    rename form1 to updateclient

--- ClassicUpdater/Program.cs
+++ ClassicUpdater/Program.cs
@@ -16,7 +16,7 @@ namespace ClassicUpdater
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new UpdateClient());
         }
     }
 }
